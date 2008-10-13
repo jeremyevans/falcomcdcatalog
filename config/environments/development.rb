257367d008bb21d02f65efb05d39099178dae4f4
Jeremy Evans <code@jeremyevans.net>
2008-10-12 19:04:05 -0700

    Don't need to specify /tmp as the host anymore

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,4 +13,4 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-DB = Sequel.postgres('falcomcdcatalog', :user=>'_postgresql', :host=>'/tmp', :logger=>Logger.new('log/development.log'))
+DB = Sequel.postgres('falcomcdcatalog', :user=>'_postgresql', :logger=>Logger.new('log/development.log'))
