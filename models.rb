73f7deb796d1d78f6e2f305644dd91bfb7eb57c9
Jeremy Evans <code@jeremyevans.net>
2010-02-06 17:19:09 -0800

    Include the blank extension, since scaffolding extensions requires it

--- models.rb
+++ models.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'logger'
 $:.unshift('/data/code/sequel/lib')
 require 'sequel'
+Sequel.extension :blank
 DB = Sequel.connect(ENV['DATABASE_URL'] || 'postgres:///fcc?user=postgres')
 ADMIN = !ENV['DATABASE_URL']
 
