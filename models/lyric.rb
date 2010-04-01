bd2b97fd725a348866cf1e7b4a68b28b3d5dfe87
Jeremy Evans <code@jeremyevans.net>
2010-04-01 13:51:14 -0700

    Switch to using one_to_one instead of one_to_many :one_to_one

--- models/lyric.rb
+++ models/lyric.rb
@@ -1,5 +1,5 @@
 class Lyric < Sequel::Model(:lyricsongs)
-  one_to_many :songs, :key=>:lyricid, :one_to_one=>true
+  one_to_one :song, :key=>:lyricid
   one_to_many :lyric_verses, :key=>:lyricsongid, :order=>[:languageid, :number]
   one_to_many :english_verses, :class_name=>'LyricVerse', :key=>:lyricsongid, :order=>:number, :conditions=>{:languageid=>1}
   one_to_many :romaji_verses, :clone=>:english_verses, :conditions=>{:languageid=>2}
@@ -10,10 +10,11 @@ class Lyric < Sequel::Model(:lyricsongs)
   many_to_one :lyricist, :class_name=>'Artist', :key=>:lyricist_id
   
   @scaffold_fields = [:rsongname, :jsongname, :joriginalsongname, :arranger, :composer, :lyricist, :vocalist]
-  @scaffold_select_order = :name
-  @scaffold_include = :songs
-  @scaffold_browse_include = :songs
-  @scaffold_associations = [:songs, :arranger, :composer, :lyricist, :vocalist, :english_verses, :romaji_verses, :japanese_verses]
+  @scaffold_select_order = :song__name
+  @scaffold_include = :song
+  @scaffold_browse_include = :song
+  @scaffold_associations = [:song, :arranger, :composer, :lyricist, :vocalist, :english_verses, :romaji_verses, :japanese_verses]
+  @scaffold_use_eager_graph = true
   
   def scaffold_name
     song.name
