e82a44eb71039d37579b6e7fdc15c9a6eb145b7e
Jeremy Evans <code@jeremyevans.net>
2010-10-14 08:35:21 -0700

    Fix autocompletion of tracks, but still not very useful

--- models/track.rb
+++ models/track.rb
@@ -3,7 +3,8 @@ class Track < Sequel::Model
   many_to_one :album, :key => :albumid
   @scaffold_fields = [:album, :discnumber, :number, :song]
   @scaffold_include = [:song, :album]
-  @scaffold_auto_complete_options = {}
+  @scaffold_auto_complete_options = {:sql_name=>'song.name'}
+  @scaffold_use_eager_graph = true
 
   def album_and_number
     "#{album.fullname}, #{"Disc #{discnumber}, " if album.numdiscs > 1}Track #{number}"
