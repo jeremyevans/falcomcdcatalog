6983cf7a7fb412bd9c22d0ecc8d701581917c535
Jeremy Evans <code@jeremyevans.net>
2017-08-26 20:03:49 -0700

    Add support for a test database in models.rb
    Also, refactor a bit and assume ruby 1.9+.

--- models.rb
+++ models.rb
@@ -1,16 +1,21 @@
-Encoding.default_internal = Encoding.default_external = 'UTF-8' if RUBY_VERSION >= '1.9'
-require 'rubygems'
+Encoding.default_internal = Encoding.default_external = 'UTF-8'
+require_relative '.env' if File.file?(File.expand_path("../.env.rb", __FILE__))
 require 'logger'
-env_file = File.expand_path("../.env.rb", __FILE__)
-require env_file if File.file?(env_file)
-
 require 'sequel'
 
-module Falcom
-  DB = Sequel.connect(ENV['FALCOMCDS_DATABASE_URL'] || ENV['DATABASE_URL'], :identifier_mangling=>false)
-  DB.extension(:freeze_datasets, :pg_array, :pg_row)
+Sequel.extension :blank, :pg_array, :pg_row, :pg_array_ops, :pg_row_ops
 
-  Sequel.extension :blank, :pg_array_ops, :pg_row_ops
+module Falcom
+  if ENV['RACK_ENV'] == 'test'
+    DB = Sequel.connect(ENV['FALCOMCDS_TEST_DATABASE_URL'])
+    logger = Logger.new($stdout)
+    logger.level = Logger::WARN
+    DB.logger = logger
+  else
+    DB = Sequel.connect(ENV['FALCOMCDS_DATABASE_URL'] || ENV['DATABASE_URL'])
+  end
+  DB.extension :pg_array, :pg_row
+
   Model = Class.new(Sequel::Model)
   Model.db = DB
   Model.def_Model(self)
@@ -18,9 +23,8 @@ module Falcom
   Model.plugin :subclasses
 
   ADMIN = ENV['FALCOMCDS_ADMIN']
-  # DB.logger = Logger.new($stdout)
 end
 
-%w'track album albuminfo artist discname game lyric lyric_verse mediatype medium publisher series song'.each{|x| require File.expand_path("../models/#{x}", __FILE__)}
+%w'track album albuminfo artist discname game lyric lyric_verse mediatype medium publisher series song'.each{|x| require_relative "models/#{x}"}
 Falcom::Model.freeze_descendents
 Falcom::DB.freeze
