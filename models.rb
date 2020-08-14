a114cb3332777870f3d58eb2fb7e3bb7dfbd4fa2
Jeremy Evans <code@jeremyevans.net>
2020-08-14 08:07:04 -0700

    Use Sequel instance_specific_default plugin when testing

--- models.rb
+++ models.rb
@@ -14,7 +14,10 @@ module Falcom
   Model.plugin :forme
   Model.plugin :subclasses
   Model.plugin :pg_auto_constraint_validations
-  Model.plugin :forbid_lazy_load if ENV['RACK_ENV'] == 'test'
+  if ENV['RACK_ENV'] == 'test'
+    Model.plugin :forbid_lazy_load
+    Model.plugin :instance_specific_default, :warn
+  end
 
   ADMIN = ENV['FALCOMCDS_ADMIN']
 end
